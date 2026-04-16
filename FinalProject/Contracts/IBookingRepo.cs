08e5e44c7c9f4a816b7277a1d60bdf546c7ad8a2
Qi Ye <qye2@charlotte.edu>
2026-04-16 01:04:12 -0400

    Implemented Booking Class/BookingRepo

--- FinalProject/Contracts/IBookingRepo.cs
+++ FinalProject/Contracts/IBookingRepo.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
+using FinalProject.Domain;
+
 namespace FinalProject.Contracts;
 
 public interface IBookingRepo
 {
-    
+    public List<Booking> GetAll();
+    public void Add(Booking b);
+    public void Delete(Booking b);
+    public List<Booking> GetBookingByRoom(Room r);
 }
--- FinalProject/Repositories/BookingRepo.cs
+++ FinalProject/Repositories/BookingRepo.cs
@@ -1,6 +1,29 @@
+using System.Collections.Generic;
 using FinalProject.Contracts;
+using FinalProject.Domain;
 
+namespace FinalProject.Repositories;
 public class BookingRepo : IBookingRepo
 {
-    
+    private readonly List<Booking> _bookings = [];
+
+    public List<Booking> GetAll()
+    {
+        return _bookings;
+    }
+
+    public void Add(Booking b)
+    {
+        _bookings.Add(b);
+    }
+
+    public void Delete(Booking b)
+    {
+        _bookings.Remove(b);
+    }
+
+    public List<Booking> GetBookingByRoom(Room r)
+    {
+        return _bookings.FindAll(b => b.Room == r);
+    }
 }
