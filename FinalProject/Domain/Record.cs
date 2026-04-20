1b45ba59d24aa4d25f8b21b97590858101ff62bf
Qi Ye <qye2@charlotte.edu>
2026-04-20 15:40:35 -0400

    Implemented the RecordService and its Interface

--- FinalProject/Domain/Record.cs
+++ FinalProject/Domain/Record.cs
@@ -5,4 +5,11 @@ public struct Record
     public int BookingId;
     public int UserId;
     public DateTime TimeStamp;
+
+    public Record(int bookingId, int userId, DateTime timeStamp)
+    {
+        BookingId = bookingId;
+        UserId = userId;
+        TimeStamp = timeStamp;
+    }
 }
