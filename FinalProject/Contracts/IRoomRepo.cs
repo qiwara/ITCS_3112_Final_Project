01d0cf258caf97c65090f49f053062db697d92b7
caileanmatthews <matthewsmcailean@gmail.com>
2026-04-27 13:38:20 -0400

    Implemented RoomRepo and interface

--- FinalProject/Contracts/IRoomRepo.cs
+++ FinalProject/Contracts/IRoomRepo.cs
@@ -1,6 +1,11 @@
+using FinalProject.Domain;
+
 namespace FinalProject.Contracts;
 
 public interface IRoomRepo
 {
-    
+    List<Room> GetAll();
+    void AddRoom(Room r);
+    Room GetById(int id);
+    void DeleteRoom(Room r);
 }
