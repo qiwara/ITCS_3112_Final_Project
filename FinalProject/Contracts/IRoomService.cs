624c740da0e436fe885e72c8ceedda71eb7e2a8c
Liam Mays <liam.mays@icloud.com>
2026-05-03 17:57:16 -0400

    Added some XML to interfaces

--- FinalProject/Contracts/IRoomService.cs
+++
@@ -1,6 +0,0 @@
-namespace FinalProject.Contracts;
-
-public interface IRoomService
-{
-    
-}
--- FinalProject/Contracts/IUserInterface.cs
+++
@@ -1,7 +0,0 @@
-namespace FinalProject.Contracts;
-
-public interface IUserInterface
-{
-    public string GetOptions();
-    public string ChooseOption(int option);
-}
